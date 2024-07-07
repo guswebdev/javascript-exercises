ac4f98ed5b0adf02d348130aa4a8de1f491da0a4
gusemmanuel <gus.desarrollo.web@gmail.com>
2024-07-07 00:41:02 -0300

    Completar el ejercicio 3 y 4 de js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let arrString = string.split("");
+  let stringReverse = "";
 
+  for (let i = arrString.length - 1; i >= 0; i--) {
+    stringReverse += arrString[i];
+  }
+
+  return stringReverse;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...removeNum) {
+  let arrArgs = Array.from(removeNum);
+  let arrFact = [...new Set(arr)];
+
+  for (item of arrArgs) {
+    let indice = arrFact.indexOf(item);
+    if (indice != -1) {
+      arrFact.splice(indice, 1);
+    }
+  }
+
+  return arrFact;
 };
 
 // Do not edit below this line
