cc5427c569fb78e9d6477508a83897dd957283b2
Gustavo Miño <gus.desarrollo.web@gmail.com>
2025-07-12 18:12:20 -0300

    Terminar los ejercicios 10 y 11 de Js

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,28 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  let result = false;
 
+  if (year % 4 === 0) {
+    result = true;
+    if (year % 100 === 0) {
+      result = false;
+      if (year % 400 === 0) {
+        result = true;
+      }
+    }
+  }
+
+  return result;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+/*
+Ejercicio 10 - leapYears
+Cree una función que determine si un año dado es bisiesto o no. Los años bisiestos se determinan mediante las siguientes reglas:
+
+Los años bisiestos son los años divisibles por cuatro (como 1984 y 2004). Sin embargo, los años divisibles por 100 no son bisiestos (como 1800 y 1900), a menos que sean divisibles por 400 (como 1600 y 2000, que sí fueron bisiestos). (Sí, todo es bastante confuso)
+
+Sugerencias
+utilice una sentencia if y && para asegurarse de que todas las condiciones se cumplen correctamente
+*/
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
-};
+//Para convertir de ºF a ºC use la fórmula:   ºC = (ºF-32) ÷ 1.8.
+const convertToCelsius = (fahrenheit) =>
+  parseFloat(((fahrenheit - 32) / 1.8).toFixed(1));
 
-const convertToFahrenheit = function() {
-};
+//Para convertir de ºC a ºF use la fórmula:   ºF = ºC x 1.8 + 32.
+const convertToFahrenheit = (celcius) =>
+  parseFloat((celcius * 1.8 + 32).toFixed(1));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
