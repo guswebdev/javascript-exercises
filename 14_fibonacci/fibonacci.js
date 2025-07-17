c35ff0d505d9fafe12e317a6ab58f1e2eeebf7fe
Gustavo Miño <gus.desarrollo.web@gmail.com>
2025-07-16 21:22:26 -0300

    Completar los ejercicios de Js de arrays y objects

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let arr = [0, 1];
 
+  if (num === 0) {
+    return 0;
+  }
+  if (num < 0) {
+    return "OOPS";
+  }
+
+  for (let i = 1; i <= num; i++) {
+    arr.push(arr[i - 1] + arr[i]);
+  }
+
+  return arr[num];
+  //console.log(arr[num]);
 };
+
+//fibonacci(10);
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,31 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
 
+const findTheOldest = function (arr) {
+  let result = arr.map((person) => {
+    if (typeof person.yearOfDeath != "number") {
+      person.age = new Date().getFullYear() - person.yearOfBirth;
+    } else {
+      person.age = person.yearOfDeath - person.yearOfBirth;
+    }
+    return person;
+  });
+
+  return result.reduce((max, objeto) => (objeto.age > max.age ? objeto : max));
 };
 
 // Do not edit below this line
